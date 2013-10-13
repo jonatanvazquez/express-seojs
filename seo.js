f9743046880c6458ac1e465da90ac485158f7984
SEO.js <hello@getseojs.com>
2013-10-13 02:05:44 +0200

    Forward HTTP headers from upstream server to requesting client, fix indentation

--- seo.js
+++ seo.js
@@ -1,13 +1,13 @@
 var http = require('http');
 
 var seojs = module.exports = function(tok) {
-    
+
     if ((typeof(tok) != 'string') && (typeof(tok) != 'undefined')) {
         throw("You must initialize express-seojs with a secret API key. For details see: https://github.com/seojs/express-seojs#readme");
     }
-    
+
     var token = tok || process.env.SEOJS_TOKEN;
-    
+
     function shouldForward(req) {
         return !(req.param('_escaped_fragment_') === undefined);
     }
@@ -31,17 +31,18 @@ var seojs = module.exports = function(tok) {
     function getSnapshot(snapshotUrl, outResponse, next) {
         http.get(snapshotUrl, function(inResponse) {
 
-          var body = "";
-          if(inResponse && inResponse.statusCode == 200) {
-              inResponse.on('data', function (data) {
-                  outResponse.write(data);
-              });
-              inResponse.on('end', function(){
-                  outResponse.end();
-              });
-          } else {
-              next();
-          }
+            if(inResponse && inResponse.statusCode == 200) {
+                outResponse.writeHead(inResponse.statusCode, inResponse.headers);
+
+                inResponse.on('data', function (data) {
+                    outResponse.write(data);
+                });
+                inResponse.on('end', function(){
+                    outResponse.end();
+                });
+            } else {
+                next();
+            }
         }).on('error', function(e) {
             next();
         });
@@ -50,15 +51,15 @@ var seojs = module.exports = function(tok) {
     function isEnabled(){
         return !!(token || process.env.SEOJS_URL);
     }
-    
-    
+
+
     // During initialization of the module:
-        
+
     if (!isEnabled()) {
         console.log("Not using express-seojs! Please set the SEOJS_TOKEN environment variable to use it.\n--- For details see: https://github.com/seojs/express-seojs#readme");
     }
-    
-    return function(req, res, next) {    
+
+    return function(req, res, next) {
         if (isEnabled() && shouldForward(req)) {
             getSnapshot(getSnapshotUrl(req), res, next);
         } else {
